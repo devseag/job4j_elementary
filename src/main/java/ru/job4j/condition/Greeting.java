42aa42d7067f38ea3f749bdc7ae7ba73937ae115
devseag <ageevvlsergey@gmail.com>
2022-11-18 19:25:31 -0400

    Fixed changes on github

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "Idea ";
+        System.out.println(idea);
+        idea = idea + "But I am a newbie ";
+        int year = 2022;
+        idea += year;
+        System.out.println(idea);
+    }
+}
