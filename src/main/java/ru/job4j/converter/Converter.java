3bcb91dcbb37394e0ef8f9e95e15ba16a4a7d0ef
devseag <ageevvlsergey@gmail.com>
2022-11-18 20:16:59 -0400

    Fixed changes on github

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,25 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+
+    public static float rubleToDollar(float value) {
+        return value / 70;
+    }
+
+    public static void main(String[] args) {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        float in2 = 120;
+        float expected2 = 2;
+        float out2 = Converter.rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("120 rubles are 2. Test result : " + passed);
+    }
+}
