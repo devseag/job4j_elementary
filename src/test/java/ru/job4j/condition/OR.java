4b4144a9fd98eec58c55af38ce58b06dc94bf362
devseag <ageevvlsergey@gmail.com>
2023-01-12 20:03:43 -0400

    Fixed changes on GitHub

---
+++ src/test/java/ru/job4j/condition/OR.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class OR {
+    public static void main(String[] args) {
+        boolean first = false;
+        boolean second = false;
+        boolean join = first || second;
+        System.out.println(join);
+    }
+}
