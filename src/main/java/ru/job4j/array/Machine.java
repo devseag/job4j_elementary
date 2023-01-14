944abf3655ba36ba556b3892a0dc60b2538ab563
devseag <ageevvlsergey@gmail.com>
2023-01-14 18:16:38 -0400

    Fixed changes on GitHub

---
+++ src/main/java/ru/job4j/array/Machine.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
+        int change = money - price;
+        for (int c : coins) {
+            while (change >= c) {
+                rsl[size] = c;
+                change -= c;
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
---
+++ src/main/java/ru/job4j/array/StringUsage.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class StringUsage {
+    public static void main(String[] args) {
+        String text = "Smith Will";
+        char[] array = text.toCharArray();
+        System.out.println(array);
+    }
+}
